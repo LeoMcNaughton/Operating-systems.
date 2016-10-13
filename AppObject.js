888b0a8b08c07fc5b12a6dda23ff9833089e06dc
LeoMcNaughton <9lmcnaughton@stpatrick.org>
2016-10-13 10:30:21 -0500

    Today I did what I could to make my operating system

---
+++ AppObject.js
@@ -0,0 +1,41 @@
+function App(n,m,s){
+
+    //Declare your instance variables here.
+    this.name = n;
+    this.memory = m;
+    this.state = s;
+
+    //Declare the instance function open here.
+    this.open = function(){
+    this.state = this.open
+    this.open = this.active
+
+  };
+    //Declare the instance function sleep here.
+    this.sleep = function(){
+    if (this.state = this.sleep){
+    this.state = this.active
+    this.memory = 2*(memory)
+  }
+}
+    //Declare the instance function active here.
+    this.active = function(){
+    this.state = active
+    ifss
+    }
+
+
+    //Declare the instance function close here.
+
+
+
+
+}
+
+//Use the constructor to create 4 App objects below.
+
+
+
+//Declare an array named appList, and place all 4 of your apps in that array.
+applist[]
+//The order does not matter.
